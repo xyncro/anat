51481f5be7047cf6bf6a4310ed344c4466c9520e
Marcus Griep <mgriep@cimpress.com>
2017-03-26 21:46:51 -0400

    Remove unnecessary files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-dotnet clean
-dotnet restore
-dotnet build -c Release --version-suffix "dev"
-dotnet test -c Release tests/Anat.Tests/Anat.Tests.fsproj
-dotnet pack -c Release --include-symbols --include-source --version-suffix "dev"
